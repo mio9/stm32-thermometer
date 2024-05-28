caf31ff3ad91d444eb7b929c29caddec163d327b
Mio Nekoya <admin@mio9.sh>
2024-05-29 05:47:48 +0800

    add more sensors

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <DHT.h>
 
 #define RS_PIN PA0
 #define RW_PIN PA1
@@ -10,28 +11,64 @@
 #define D5_PIN PA4
 #define D6_PIN PA5
 #define D7_PIN PA6
-#define ONEWIRE_BUS PB4
+#define ONEWIRE_BUS PB5
+
+#define DHTPIN PB4
+#define DHTTYPE DHT11
 
 LiquidCrystal lcd(RS_PIN, ENABLE_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
+
+DHT dht(DHTPIN, DHTTYPE);
+
 OneWire oneWire(ONEWIRE_BUS);
 DallasTemperature sensor(&oneWire);
 
 void setup()
 {
   sensor.begin();
+  dht.begin();
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  lcd.print("Temperature");
 }
 
 void loop()
 {
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // old sensor 2
   sensor.requestTemperatures();
-  float temp = sensor.getTempCByIndex(0);
-  // put your main code here, to run repeatedly:
+  float t2 = sensor.getTempCByIndex(0);
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  float t_avg = (t + t2) / 2;
+
+  float hic = dht.computeHeatIndex(t_avg, h, false);
+
+  lcd.setCursor(0, 0);
+  // print the temperature in Celsius
+  lcd.print("T:");
+  lcd.print(t);
+  lcd.print(" ");
+  lcd.print("H:");
+  lcd.print(h);
+  lcd.print("%");
+
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(temp);
-  lcd.print("C");
-  delay(1000);
+  // lcd.print(temp);
+  lcd.print("HI:");
+  lcd.print(hic);
+  lcd.print(" ");
+  lcd.print("D:");
+  lcd.print(t-t2);
+
+  delay(1500);
 }
